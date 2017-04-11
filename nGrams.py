92b774724bb19bb53489163232b5ca7d3fe57165
Asnowman7 <asnowman7@gmail.com>
2017-04-11 14:39:29 -0400

    Cleaning up project #3
    renamed tagging.py to tagger.py, and wrote bigram generation function
    for nGrams.py

--- nGrams.py
+++ nGrams.py
@@ -3,3 +3,25 @@ Morphological Parser:
     nGram functions
 '''
 
+from nltk.corpus import brown
+
+corpus = brown.tagged_words()
+
+def corpusBigrams():
+    bigrams = []
+    for i in range(1, len(corpus)):
+        tally = 1
+        newBigram = [corpus[i-1], corpus[i], tally]
+        if newBigram in bigrams: #if newBigram is already in bigram list
+            for b in bigrams:
+                if newBigram == b:
+                    b[2] += 1 #increment bigram tally
+        else:
+            bigrams.append(newBigram)
+    return bigrams
+
+
+#corpus = [["blah", "PP"], ["tally", "NN"], ["blah", "PP"], ["tally", "NN"], ["unamused", "JJ"]]
+
+print corpusBigrams()
+            
