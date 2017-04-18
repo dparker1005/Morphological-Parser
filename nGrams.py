da897a2278bf69e2d39bbefb1ec4f35db761c91b
dlparker <dlparker@haverford.edu>
2017-04-18 15:56:21 -0400

    optimized bigram creation

--- nGrams.py
+++ nGrams.py
@@ -4,6 +4,7 @@ Morphological Parser:
 '''
 
 from nltk.corpus import brown
+from operator import itemgetter, attrgetter, methodcaller
 
 corpus = brown.tagged_words()
 
@@ -11,19 +12,42 @@ def corpusBigrams():
     #if bigramList != []:
       #  return bigramList
     bigrams = []
+    corpLen = len(corpus)
     for i in range(1, len(corpus)):
+	tempStr = str(i)+"/"+str(corpLen)
+	print tempStr
         tally = 1
         newBigram = [corpus[i-1], corpus[i], tally]
-        if newBigram in bigrams: #if newBigram is already in bigram list
-            for b in bigrams:
-                if newBigram == b:
-                    b[2] += 1 #increment bigram tally
-        else:
+        #if newBigram in bigrams: #if newBigram is already in bigram list
+        #    for b in bigrams:
+        #        if newBigram == b:
+        #            b[2] += 1 #increment bigram tally
+        #else:
             #print newBigram
-            bigrams.append(newBigram)
+        bigrams.append(newBigram)
+
+    print("sorting")
+    sortedBigrams = sorted(bigrams, key=itemgetter(0,1))
+    
+    toReturn = []
+    bigramToMatch = sortedBigrams[0]
+    tally = 1
+    listLen = len(sortedBigrams)
+    for i in range(1, len(sortedBigrams)):
+	tempStr = str(i)+"/"+str(listLen)
+	print(tempStr)
+	if sortedBigrams[i] == bigramToMatch:
+	    tally += 1
+	else:
+	    temp = bigramToMatch
+	    temp[2] = tally
+	    toReturn += [temp]
+	    bigramToMatch = sortedBigrams[i]
+	    tally = 1 
+
     file1 = open('bigramGrammar.txt', 'w')
-    for b in bigrams:
-        file1.write(b)
+    for b in toReturn:
+        file1.write(str(b)+"\n")
     file1.close()
     return bigrams
 
