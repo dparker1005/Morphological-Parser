00a7738593b0a7119349883e397c82bacea4cc39
Asnowman7 <asnowman7@gmail.com>
2017-04-18 14:40:38 -0400

    Extended nGrams
    added function that returns all bigrams containing input word and text
    file to save corpus grammar

--- nGrams.py
+++ nGrams.py
@@ -8,6 +8,8 @@ from nltk.corpus import brown
 corpus = brown.tagged_words()
 
 def corpusBigrams():
+    #if bigramList != []:
+      #  return bigramList
     bigrams = []
     for i in range(1, len(corpus)):
         tally = 1
@@ -17,11 +19,24 @@ def corpusBigrams():
                 if newBigram == b:
                     b[2] += 1 #increment bigram tally
         else:
+            #print newBigram
             bigrams.append(newBigram)
+    file1 = open('bigramGrammar.txt', 'w')
+    for b in bigrams:
+        file1.write(b)
+    file1.close()
     return bigrams
 
+def wordToBigram(word):
+    #returns bigrams that contain input word from list of bigrams
+    result = []
+    for b in bigramList:
+        if (word in b[0]) or (word in b[1]):
+            result.append(b)
+    return result
 
 #corpus = [["blah", "PP"], ["tally", "NN"], ["blah", "PP"], ["tally", "NN"], ["unamused", "JJ"]]
 
-print corpusBigrams()
+corpusBigrams()
+print wordToBigram("amuse")
             
