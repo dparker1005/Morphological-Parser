58f825171184026e0c32cda4cfcafbc5b8a42b1c
dlparker <dlparker@haverford.edu>
2017-05-05 13:23:12 -0400

    updated bigramGrammar.txt

--- nGrams.py
+++ nGrams.py
@@ -8,23 +8,23 @@ from operator import itemgetter, attrgetter, methodcaller
 bigramList = []
 
 def corpusBigrams():
-	corpus = nltk.corpus.brown.tagged_words()
+	from nltk.corpus import brown
+	corpus = brown.tagged_words()
 	bigrams = []
 	corpLen = len(corpus)
 	for i in range(1, len(corpus)):
 		tempStr = str(i)+"/"+str(corpLen)
 		print tempStr
-        tally = 1
-        newBigram = [corpus[i-1], corpus[i], tally]
-        bigrams.append(newBigram)
-
+        	tally = 1
+        	newBigram = [corpus[i-1], corpus[i], tally]
+        	bigrams.append(newBigram)
 	print("sorting")
 	sortedBigrams = sorted(bigrams, key=itemgetter(0,1))
-			
 	toReturn = []
 	bigramToMatch = sortedBigrams[0]
 	tally = 1
 	listLen = len(sortedBigrams)
+	print len(sortedBigrams)
 	for i in range(1, len(sortedBigrams)):
 		tempStr = str(i)+"/"+str(listLen)
 		print(tempStr)
@@ -36,7 +36,6 @@ def corpusBigrams():
 			toReturn += [temp]
 			bigramToMatch = sortedBigrams[i]
 			tally = 1
-
 	file1 = open('bigramGrammar.txt', 'w')
 	for b in toReturn:
 		if(not '-' in b[0][0] and not '-' in b[1][0]):
